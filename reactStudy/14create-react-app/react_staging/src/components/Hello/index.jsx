0d3e3d7199e982fcdd84d02f941f456aa3ba1f69
anxk <anxk@yonyou.com>
2020-12-28 22:13:12 +0800

    添加react 脚手架

---
+++ reactStudy/14create-react-app/react_staging/src/components/Hello/index.jsx
@@ -0,0 +1,7 @@
+import React,{Component} from 'react'
+import './index.css'
+export default class Hello extends Component{
+    render(){
+        return <h2 className="title">Hello，react</h2>
+    }
+}
