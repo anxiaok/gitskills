9422bd066f2761a44eaf5debf7d3a0fed3732ffa
anxk <anxk@yonyou.com>
2020-12-31 00:41:47 +0800

    头部添加功能

--- reactStudy/14create-react-app/react_staging/src/App.jsx
+++ reactStudy/14create-react-app/react_staging/src/App.jsx
@@ -5,12 +5,30 @@ import List from './components/List'
 import Footer from './components/Footer'
 // 创建并暴露App组件
 export default class App extends Component{
+    // 初始化状态
+    state = {
+        todos:[
+            {id:'001',name:'吃饭',done:true},
+            {id:'002',name:'睡觉',done:true},
+            {id:'003',name:'打代码',done:false},
+            {id:'004',name:'逛街',done:true},
+        ]
+    }
+    // 用于添加一个todo,接收的参数是todo对象
+    addTodo = (todoObj)=>{
+        const {todos} = this.state;
+        const newTodos = [todoObj,...todos];
+        this.setState({
+            todos:newTodos
+        });
+    }
     render(){
+        const {todos} = this.state;
         return (
             <div className="todo-container">
                 <div className="todo-wrap">
-                    <Header />
-                    <List />
+                    <Header addTodo={this.addTodo} />
+                    <List todos={todos}/>
                     <Footer />
                 </div>
             </div>
