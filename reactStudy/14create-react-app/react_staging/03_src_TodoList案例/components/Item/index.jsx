9d5425d7a5e671c77cbd2301ce3b6946e1f3d70e
anxk <anxk@yonyou.com>
2021-01-01 21:35:43 +0800

    完成todoList功能

--- reactStudy/14create-react-app/react_staging/03_src_TodoList案例/components/Item/index.jsx
+++ reactStudy/14create-react-app/react_staging/03_src_TodoList案例/components/Item/index.jsx
@@ -13,16 +13,23 @@ export default class Item extends Component {
             updateTodo(id,event.target.checked);
         }
     }
+    // 删除的回调
+    handleDelete = (id)=>{
+        if(window.confirm('确定删除吗？')){
+            const {deleteTodo} = this.props;
+            deleteTodo(id);
+        }
+    }
     render() {
         const {id,name,done} = this.props
         const {mouse} = this.state;
         return (
             <li style={{backgroundColor:mouse? '#ddd':'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
                 <label>
-                    <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
+                    <input type="checkbox" checked={done} onChange={this.handleCheck(id)}/>
                     <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{display:mouse ? 'block':'none'}}>删除</button>
+                <button onClick={()=>this.handleDelete(id)} className="btn btn-danger" style={{display:mouse ? 'block':'none'}}>删除</button>
             </li>
         )
     }
