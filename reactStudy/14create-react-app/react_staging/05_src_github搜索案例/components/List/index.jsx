390b438ea22880e3673c1df7ac6077c3942a9cf3
anxk <anxk@yonyou.com>
2021-01-02 18:44:38 +0800

    添加fetch和github案例（完成）

--- reactStudy/14create-react-app/react_staging/05_src_github搜索案例/components/List/index.jsx
+++ reactStudy/14create-react-app/react_staging/05_src_github搜索案例/components/List/index.jsx
@@ -1,22 +1,8 @@
 import React, { Component } from 'react'
-import PubSub from 'pubsub-js'
+
 export default class List extends Component {
-    state = {
-        users:[],// 初始化状态，users初始值为数组
-        isFirst:true,// 是否为第一次打开页面
-        isLoading:false,//标识是否处于加载中
-        err:'',// 存储请求相关的错误信息
-    }
-    componentDidMount(){
-        this.token = PubSub.subscribe('atguigu',(msg,stateObj)=>{
-            this.setState(stateObj);
-        })
-    }
-    componentWillUnmount(){
-        PubSub.unsubscribe(this.token);
-    }
     render() {
-        const {users,isFirst,isLoading,err} = this.state;
+        const {users,isFirst,isLoading,err} = this.props;
         return (
             <div className="row">
                 {
