80803696fd7ccdc211be29974ecf04649a7da1dd
anxk <anxk@yonyou.com>
2021-01-02 21:15:34 +0800

    history和hash值的介绍

--- reactStudy/14create-react-app/react_staging/src/App.jsx
+++ reactStudy/14create-react-app/react_staging/src/App.jsx
@@ -1,16 +1,11 @@
 // 创建“外壳”组件App 分别暴露和统一暴露
 import React,{Component} from 'react'
-import Search from './components/Search'
-import List from './components/List'
 // 创建并暴露App组件
 export default class App extends Component{
-    
-    
     render(){
         return (
-            <div className="container">
-                <Search/>
-                <List/>
+            <div>
+                app...
             </div>
         )
     }
